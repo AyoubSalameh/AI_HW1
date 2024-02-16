8dff9aeed22e6295c6384875488d100848a97a95
Tal-Shab <tal.shab@campus.technion.ac.il>
2024-02-16 10:32:11 +0200

    move the 'inheritance' of the node state from its parent to the Ctor, and split the code to smaller functions

--- Algorithms.py
+++ Algorithms.py
@@ -8,19 +8,24 @@ import heapdict
 
 
 class Node:
-    def __init__(self, state, step, cost=0, terminated=False, parentNode=None) -> None:
-        self.state = state
+    def __init__(self, state, step=None, cost=0, terminated=False, parent_node=None) -> None:
+        if parent_node is not None:
+            self.state = (state[0], parent_node.state[1], parent_node.state[2])
+        else:
+            self.state = state
+
         self.step = step  # this is the action taken to reach the current node
         self.cost = cost
         self.terminated = terminated
-        self.parentNode = parentNode
-        self.expanded = False
+        self.parentNode = parent_node
         self.total_cost = cost
-        if parentNode is not None:
-            self.total_cost += parentNode.total_cost
+        if parent_node is not None:
+            self.total_cost += parent_node.total_cost
 
-    # TODO: check if we need a printing method.
     def __repr__(self) -> str:
+        """
+            overloads the print to print the state of the Node when calling print(node)
+        """
         return f"{self.state}"
 
 
@@ -29,12 +34,12 @@ class Agent:
         self.env = None
 
     @staticmethod
-    def solution(node: Node) -> Tuple[List[int], int]:
+    def solution(final_node: Node) -> Tuple[List[int], int]:
         actions = []
-        total_cost = node.total_cost
-        while node.parentNode is not None:
-            actions.append(node.step)
-            node = node.parentNode
+        total_cost = final_node.total_cost
+        while final_node.parentNode is not None:
+            actions.append(final_node.step)
+            final_node = final_node.parentNode
         actions.reverse()
         return actions, total_cost
 
@@ -42,58 +47,49 @@ class Agent:
 class BFSAgent(Agent):
     def __init__(self) -> None:
         super().__init__()
+        self.open: List[Node] = []  # open contains nodes
+        self.close: List[Tuple[int, int, int]] = []  # close contains states
 
-    def animation(self, epochs: int, state: int, action: List[int], total_cost: int) -> None:
-        clear_output(wait=True)
-        print(self.env.render())
-        print(f"Timestep: {epochs}")
-        print(f"State: {state}")
-        print(f"Action: {action}")
-        print(f"Total Cost: {total_cost}")
-        time.sleep(1)
+    def update_node_state_if_db(self, node: Node, curr_cell: int) -> None:
+        if curr_cell == self.env.d1[0]:
+            node.state = (node.state[0], True, node.state[2])
+        if curr_cell == self.env.d2[0]:
+            node.state = (node.state[0], node.state[1], True)
 
-    def search(self, env: DragonBallEnv) -> Tuple[List[int], float, int]:
+    def initialize(self, env) -> None:
         self.env = env
         self.env.reset()
-        # open contains nodes
-        open = []
-        # close contains states
-        close = []
+        self.open = []
+        self.close = []
 
+    def search(self, env: DragonBallEnv) -> Tuple[List[int], float, int]:
+        self.initialize(env)
+        # not sure if it is legal for the bool to be at the start, but just in case
         curr_state = self.env.get_initial_state()
-        # TODO: check if cost for initial state is 0 or 1
-        curr_node = Node(curr_state, None, 0, False, None)
-        open.append(curr_node)
+        curr_node = Node(curr_state)
+        self.update_node_state_if_db(curr_node, curr_state[0])
+        self.open.append(curr_node)
 
-        # initial state will not be the final goal since we are promised to have 2 balls to collect.
-
-        while len(open) != 0:
-            curr_node = open.pop(0)
-            print("curr node:", curr_node)
-            close.append(curr_node.state)
+        while len(self.open) != 0:
+            curr_node = self.open.pop(0)
+            # print("curr node:", curr_node)
+            self.close.append(curr_node.state)
             for action, (succ_state, cost, terminated) in env.succ(curr_node.state).items():
                 succ_node = Node(succ_state, action, cost, terminated, curr_node)
-                succ_node.state = (succ_node.state[0], curr_node.state[1], curr_node.state[2])
-                #print("operation " + str(action))
-                """checking if we reached a db """
-                if succ_state[0] == self.env.d1[0]:
-                    succ_node.state = (succ_node.state[0], True, curr_node.state[2])
-                if succ_state[0] == self.env.d2[0]:
-                    succ_node.state = (succ_node.state[0], curr_node.state[1], True)
+                self.update_node_state_if_db(succ_node, succ_state[0])
 
-
-
-                if (succ_node.state not in close) and (succ_node.state not in [ item.state for item in open]):
+                if (succ_node.state not in self.close) and (succ_node.state not in [item.state for item in self.open]):
                     if self.env.is_final_state(succ_node.state):
-                        print("************FINAL STATE*************" + str(succ_state))
+                        # print("************FINAL STATE*************" + str(succ_node.state))
                         (path, total_cost) = self.solution(succ_node)
-                        return (path, total_cost, len(close))
-                    #not a hole and not same state
+                        return path, total_cost, len(self.close)
+
                     if terminated or succ_state == curr_node.state:
                         continue
-                    open.append(succ_node)
 
-        return ([1],1,1)
+                    self.open.append(succ_node)
+
+        return None
 
 
 class WeightedAStarAgent():
